c688468167efa72d5b31e7401cf4fec24798fff0
ÖMER KINCAL <202804010@ogr.cbu.edu.tr>
2023-11-16 11:07:20 +0300

    version updated to 3.0.3, spring security added in two different way

---
+++ src/main/java/com/nova/todomanagmentproject/config/SpringSecurityConfig.java
@@ -0,0 +1,56 @@
+package com.nova.todomanagmentproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfig {
+
+
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests(authorize -> {
+                    //authorize.requestMatchers(HttpMethod.POST , "/api/**").hasRole("ADMIN")
+                    //authorize.requestMatchers(HttpMethod.PUT , "/api/**").hasRole("ADMIN")
+                    //authorize.requestMatchers(HttpMethod.DELETE , "/api/**").hasRole("ADMIN")
+                    //authorize.requestMatchers(HttpMethod.GET, "/api/**").hasAnyRole("ADMIN" , "USER")
+                    //authorize.requestMatchers(HttpMethod.GET,"/api/**").permitAll()
+                    authorize.anyRequest().authenticated();
+                }).httpBasic(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails omer = User.builder()
+                .username("omer")
+                .password(passwordEncoder().encode("omer"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(omer, admin);
+    }
+}
--- src/main/java/com/nova/todomanagmentproject/controller/TodoController.java
+++ src/main/java/com/nova/todomanagmentproject/controller/TodoController.java
@@ -5,28 +5,31 @@ import com.nova.todomanagmentproject.service.TodoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("todos")
+@RequestMapping("api/todos")
 @RequiredArgsConstructor
 public class TodoController {
     private final TodoService service;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<TodoDto> save(@RequestBody TodoDto todoDto){
         TodoDto dto = service.save(todoDto);
         return new ResponseEntity<>(dto, HttpStatus.CREATED);
     }
-
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("{id}")
     public ResponseEntity<TodoDto> getTodo(@PathVariable(name = "id") String todoId){
         TodoDto dto = service.getTodo(todoId);
         return new ResponseEntity<>(dto, HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("{id}")
     public ResponseEntity<TodoDto> updateTodo(@PathVariable(name = "id") String todoId,
                                               @RequestBody TodoDto todoDto){
@@ -34,23 +37,27 @@ public class TodoController {
         return new ResponseEntity<>(dto, HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @GetMapping
     public ResponseEntity<List<TodoDto>> getAllTodos(){
         return new ResponseEntity<>(service.getAllTodos(), HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("{id}")
     public ResponseEntity<String> delete(@PathVariable(name = "id") String todoId){
         service.deleteTodo(todoId);
         return new ResponseEntity<>("Deleted", HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @PatchMapping("{id}/complete")
     public ResponseEntity<TodoDto> completeTodo(@PathVariable(name = "id") String todoId){
         TodoDto dto = service.completeTodo(todoId);
         return new ResponseEntity<>(dto, HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @PatchMapping("{id}/in-complete")
     public ResponseEntity<TodoDto> inCompleteTodo(@PathVariable(name = "id") String todoId){
         TodoDto dto = service.inCompleteTodo(todoId);
